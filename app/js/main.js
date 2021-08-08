5ff4280f9ed8750376d9f035060fd092b25f9d69
JuLta <juliettaran@gmail.com>
2021-08-08 12:51:15 +0300

    added page product-list1, added page product-list2

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,35 @@
 $ (function(){
+
+  $(".star").rateYo({
+    starWidth: "12px",
+    readOnly: true,
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    spacing: "6px",
+  });
+
+  $(".shop-content__star").rateYo({
+    starWidth: "20px",
+    readOnly: true,
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    spacing: "12px",
+  });
+  
+
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    onStart: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data) {
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+  });
+
   $('.slider-top__inner').slick({
     dots: true,
     arrows: false,
@@ -8,8 +39,16 @@ $ (function(){
   });
 
   
-  var mixer = mixitup('.products__content');
-  var mixer = mixitup('.content');
+  var containerEL1 = document.querySelector('[data-ref="products-1"]');
+  var containerEL2 = document.querySelector('[data-ref="design-1"]');
+
+  var config = {
+    controls : {
+      scope: 'local'
+    }
+  };
+  var mixer1 = mixitup(containerEL1, config);
+  var mixer2 = mixitup(containerEL2, config);
   
 });
 
