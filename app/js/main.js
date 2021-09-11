c6b5ab35e30d3a3a1ca6cd56ef59316f4efc1afb
JuLta <juliettaran@gmail.com>
2021-09-11 11:28:41 +0300

    adaptive pages detalis-product

--- app/js/main.js
+++ app/js/main.js
@@ -36,6 +36,29 @@ $ (function(){
     slidesToShow: 4,
     slidesToScroll: 4,
     infinite: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 790,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 530,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+    ]
   }); 
 
   $('.detalis-slide__thumb').slick({
@@ -51,6 +74,14 @@ $ (function(){
     draggable: false,
     arrows: false,
     fade: true,
+    responsie: [
+      {
+        breakpoint: 1200,
+        settings: {
+          draggable: true,
+        }
+      },
+    ]
   });
 
   $(".star").rateYo({
